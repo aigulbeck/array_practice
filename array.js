5259473ff3c001b98bbd1db06a55407efafe2863
Aigul Beck <aigul.beck@gmail.com>
2023-08-30 16:51:41 -0500

    Declare a variable that stores an Array

---
+++ array.js
@@ -0,0 +1,8 @@
+// Declare a variable that stores an Array of at least 4 Strings.
+var friends = ["Monica", "Joey", "Rachel", "Ross"]
+
+// Declare a variable that stores an Array of at least 4 Numbers.
+var pages = [22, 44, 66, 88]
+
+// Declare a variable that stores an Array of at least 4 Booleans.
+var isHungry = [true, true, false, true]
